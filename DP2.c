1ae935527eee09ad97c8702568446c4c4960edde
Nick Kyparissas <nkyparissas@users.noreply.github.com>
2017-03-26 17:51:24 +0300

    Needs cleaning up from dbging msges

--- DP2.c
+++ DP2.c
@@ -17,6 +17,7 @@
 
 typedef struct node{
     float max_value; //maximum value of objects we can get at this node of the grid 
+    float weight_for_max_value;
     int previous_level_node; //node from the previous level.Transition from this node gave the max value 
     int number_of_items_taken; //how many items of previous node were taken (o elegxos)
 } node;
@@ -56,58 +57,85 @@ int main(){
 		}while((K<=0)||(B<0));	
 	}
 		
-		
+	for (i=0; i<K; i++) printf("b[%d] = %f, w[%d] = %f\n", i, b[i], i, w[i]);	
 	//The Dynamic Programming graph will have K number of levels. 
 	int *NodesPerLevel = (int *)malloc(K * sizeof(int));
 	
 	//Each level will have as many nodes as the number of the item k the load can carry 
 	//(how many items of weight b[k] can fit in total load's weight B)
 	NodesPerLevel[0] = 1;
-	for (k=1; k<K; k++)
-		NodesPerLevel[k] = (B/b[k])+1;
-	
+	for (k=1; k<=K; k++){
+		NodesPerLevel[k] = (B/b[k-1])+1;
+		printf("NodesPerLevel[%d]: %d\n", k, NodesPerLevel[k]);
+	}
 	/* Allocate memory for node grid*/
-	node ** grid = (node **)malloc(K * sizeof(node *));
- 	for (i = 0; i < K; i++)  // was: i <= K ? If we want this: malloc needs to change
+	node ** grid = (node **)malloc((K+1) * sizeof(node *));
+ 	for (i = 0; i <= K; i++)  // was: i <= K ? If we want this: malloc needs to change
 		assert(grid[i] = (node *)malloc(NodesPerLevel[i]* sizeof(node)));
 
+	printf("\nGRID:\n");
 	/*Initialize nodes*/
-	for (i = 0; i < K; i++){  // was: i <= K ? If we want this: malloc needs to change
+	for (i = 0; i <= K; i++){  // was: i <= K ? If we want this: malloc needs to change
 		for (j = 0; j < NodesPerLevel[i]; j++){ // was: i++
 			grid[i][j].max_value = 0;
 			grid[i][j].previous_level_node = 0; 
 			grid[i][j].number_of_items_taken = 0;
+			grid[i][j].weight_for_max_value = 0;
+			printf("%d %d %d, ", grid[i][j].max_value, grid[i][j].previous_level_node, grid[i][j].number_of_items_taken);
 		}
+		printf("\n");
 	}
 
+
+	fflush(stdin);
+	getchar();
+	/////////////////
 	/*
 	* Dynamic Programming
 	*/
-	int transition_value;
-	for (i = 0; i <= K; i++){
-		for (j = 0; j <= NodesPerLevel[i] - 1; j++){ 
-			for (m = 0; m <= NodesPerLevel[j+1]; m++){
+	float transition_value;
+	for (i = 1; i <= K; i++){
+		;
+		for (m = 0; m < NodesPerLevel[i-1]; m++){
+			
+			for (j = 0; j < NodesPerLevel[i]; j++){ 	
+				printf("level: %d, node: %d, node from previous level: %d\n", i, j, m);
+				
 				//calculate transition
-				transition_value = grid[i][j].max_value + (m*w[i]);
-				//check that transition is withind limits
-				if (transition_value <= B){
-					//if transition is better than before update node at level j+1
-					if(transition_value >= grid[i+1][j+m].max_value){
-						grid[i+1][j+m].max_value = transition_value;
-						grid[i+1][j+m].previous_level_node = j;
-						grid[i+1][j+m].number_of_items_taken = m;
+				if (grid[i-1][m].weight_for_max_value + j*b[i-1] <= B){
+					transition_value = j*w[i-1] + grid[i-1][m].max_value;
+					printf("transition_value = %d*w[%d] + %d*w[%d] = %d*%f + %d*%f = %f\n", j-m, i, m, i-1, j-m, w[i], m, w[i-1], transition_value);
+					//check that transition is withind limits
+						//if transition is better than before update node at level j+1
+						if(transition_value >= grid[i][j].max_value){
+							grid[i][j].max_value = transition_value;
+							grid[i][j].weight_for_max_value = grid[i-1][m].weight_for_max_value + j*b[i-1];
+							grid[i][j].previous_level_node = m;
+							grid[i][j].number_of_items_taken = j;
+							printf("new max: %f from %d\n", grid[i][j].max_value, m);
 					}
+					//}
 				}
+				else printf("WEIGHT WARNING\n");
+				// printf("WEIGHT WARNING: %d \n", grid[i-1][m].weight_for_max_value + j*b[i-1]);
 			}
 		}
 	}
 
+	float max = -1;
+	int max_position = -1;
+	for(j = 0; j<NodesPerLevel[K]; j++){
+		if (grid[K][j].max_value > max){
+			max = grid[K][j].max_value;	
+			max_position = j;
+		}
+	}
+	
 	/* Print result */
-	printf("best value is : %d \n",grid[K][NodesPerLevel[K]-1].max_value );
-	int arrow = NodesPerLevel[K];
-	for(i = K; i>=0; i--){
-		printf("taken %d objects of type %d \n", grid[i][arrow].number_of_items_taken, i-1 );
-		arrow = grid[i][arrow].previous_level_node;
+	printf("best value is : %f \n", max );
+	for(i = K; i>0; i--){
+		printf("taken %d objects of type %d \n", grid[i][max_position].number_of_items_taken, i-1 );
+		max_position = grid[i][max_position].previous_level_node;
 	}
 	
 	
